25d67493cc6549783213a5ccde9b380cf21e06b9
pratimpa <pratimpa@in.ibm.com>
2025-04-16 13:12:37 +0530

    created solution

---
+++ WebAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
