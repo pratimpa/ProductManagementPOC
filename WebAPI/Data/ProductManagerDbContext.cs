87181341e7c28097ddbc11210c37f8d8f840b014
pratimpa <pratimpa@in.ibm.com>
2025-04-17 12:38:22 +0530

    add database context

---
+++ WebAPI/Data/ProductManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class ProductManagerDbContext :DbContext
+    {
+        public ProductManagerDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,18 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<ProductManagerDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductMgrConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
 
 app.UseHttpsRedirection();
 
