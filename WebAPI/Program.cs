2ded605fa8971762ddd4602393416ecbacf21064
Merrin E <merrie11@in.ibm.com>
2025-04-21 12:50:04 +0530

    Added Category CRUD API and tested via Swagger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,14 +1,23 @@
 using Microsoft.EntityFrameworkCore;
-using WebAPI.Data;
+using WebAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); 
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<ProductManagerDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductMgrConnectionString")));
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
